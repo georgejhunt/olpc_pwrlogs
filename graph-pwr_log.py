0a430c1aefc354b3d36685fbf09b541b75b2ee8e
Richard A. Smith <richard@laptop.org>
2012-03-30 21:26:03 -0400

    graph-pwr_log:  Inprovements and new graphs for processing batcap logs

--- graph-pwr_log.py
+++ graph-pwr_log.py
@@ -11,24 +11,27 @@ import sys
 import csv
 import os
 import traceback
-from numpy import *
-from matplotlib import mlab
+import numpy as np
+import matplotlib
+import matplotlib.mlab as mlab
 from matplotlib.ticker import MultipleLocator
-from pylab import figure, show
+from pylab import figure, show, normpdf
 import matplotlib.pyplot as plt
 import argparse
 from datetime import datetime, date, time
 from dateutil import tz, parser
 from matplotlib.backends.backend_pdf import PdfPages
+from scipy import interpolate
 
 class pwr_trace:
 	def __init__(self):
 
 		self.header = {}
 
-		self.Tz	     	= 0.
-		self.ACRz	= 0.
-		self.Wh_sum	= 0.
+		self.Tz	     	 = 0.
+		self.ACRz	 = 0.
+		self.Wh_sum	 = 0.
+		self.interval_Wh = 0.
 
 		# Small arrry for a place holder will will replace this once we have built the data list
 		self.darray	= zeros(3)
@@ -63,11 +66,16 @@ class PwrLogfile:
 		self.Zavg    = 9
 
 		# Small arrry for a place holder will will replace this once we have built the data list
-		self.darray	= zeros(3)
+		self.darray	= np.zeros(3)
 		self.min_sample_interval = 0
 		self.charge_limit=30
 		self.enable_charge_limit=False
 		self.local_tz = tz.tzutc()
+		# The wattage calcs should never be outside these ranges.  If they are
+		# then there is some sort of error.
+		self.max_watts_limit = 20
+		self.min_watts_limit = -15
+		self.max_Th	     = 50
 
 	def convert_data(self,row):
 		converted = [0.,0.,0.,0.,0.,0.]
@@ -85,7 +93,7 @@ class PwrLogfile:
 	        # Old versions of the logging script have this number as an unsinged 16-bit
                 # But its really a 2's complement so you have to fixup to make the math work across
         	# a rollover.
-		if self.header['XOVER'] == '1.5' or self.header['KERNAPI'] == '2' :
+		if self.header['XOVER'] == '1.5' or self.header['KERNAPI'] == '2':
                         # in gen 1.5 this value is reported converted into uAh
                         converted[self.ACR] = float(row[self.ACR]) / 1000.0
 		else:
@@ -97,11 +105,11 @@ class PwrLogfile:
         	                if (intval & 0x8000):
                         	        intval = -((~intval & 0xffff) +1)
 
-	                        converted[self.ACR] = float(intval)*self.ACR2mAh
+				converted[self.ACR] = float(intval)*self.ACR2mAh
 
 		return converted
 
-	def process_data(self,converted, converted_prev):
+	def process_data(self,converted, converted_prev, skip_short_checks=False):
 		result = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.]
 		dt_sample = datetime.fromtimestamp(converted[self.SEC],tz.tzutc())
 		dt_tz = dt_sample.astimezone(self.local_tz).timetuple()
@@ -116,17 +124,25 @@ class PwrLogfile:
 		# If either of these are small then we want to skip to the next reading
 		# because of the error associated with small values
 		if abs(result[self.Deltat]) < self.min_sample_interval or abs(DeltaACR) < .5:
-			return (result,1)
+			if not skip_short_checks:
+				return (result,1)
 
 	        result[self.Iavg]    = DeltaACR / (result[self.Deltat] / 3600)
 	        result[self.NetACR]  = converted[self.ACR] - self.ACRz
 	        result[self.Vavg]    = (converted[self.Vb] + converted_prev[self.Vb]) / 2
         	result[self.Watts]   = result[self.Vavg] * (result[self.Iavg] / 1000)
 
-		if result[self.Watts] > self.charge_limit and self.enable_charge_limit:
-			result[self.Watts] = self.charge_limit
+		if result[self.Watts] > self.max_watts_limit or result[self.Watts] < self.min_watts_limit:
+			return (result,2)
 
-	        result[self.Wh]      = self.Wh_sum + (result[self.Watts] * result[self.Deltat] / 3600)
+		if result[self.Th] > self.max_Th or result[self.Th] < 0:
+			return (result,3)
+
+		# Keep a copy of the sample Wh so we can adjust things correctly when computing
+		# the final interval
+		self.interval_Wh     =  (result[self.Watts] * result[self.Deltat] / 3600)
+	        result[self.Wh]      = self.Wh_sum + self.interval_Wh
+
 		if result[self.Th] != 0.0:
 			result[self.Wavg]    = result[self.Wh] / result[self.Th]
 		else:
@@ -147,35 +163,41 @@ class PwrLogfile:
 		# header formats
 		self.header['XOVER'] = '1'
 		self.header['KERNAPI'] = '0'
-        	for row in reader:
-                	if not row:
-                        	continue
-		        if row[0] == '<StartData>':
-        	                break
+		try:
+			for row in reader:
+				if not row:
+					continue
+				if row[0] == '<StartData>':
+					break
 
-                        if row[0].startswith('DATE:'):
-				# Dates can have commas and they get pased as csv so reconstruct
-				# the full string.
-				dstring = ''
-				for each in row:
-					dstring += each
-				dcolon = dstring.find(":")+1
-				dstring = dstring[dcolon:]
-                                rundate = parser.parse(dstring,fuzzy=True)
-				self.header['DATE'] = rundate
-				continue
+				if row[0].startswith('DATE:'):
+					# Dates can have commas and they get pased as csv so reconstruct
+					# the full string.
+					dstring = ''
+					for each in row:
+						dstring += each
+					dcolon = dstring.find(":")+1
+					dstring = dstring[dcolon:]
+					rundate = parser.parse(dstring,fuzzy=True)
+					self.header['DATE'] = rundate
+					continue
 
-	                try:
-                        	values = row[0].split(':')
-				if len(values) > 1:
-					self.header[values[0]] = values[1].strip()
-				elif len(values) > 0:
-					self.header[values[0]] = ''
-                	except:
-				print 'Error in header: %s' % (filename)
+				try:
+					values = row[0].split(':')
+					if len(values) > 1:
+						self.header[values[0]] = values[1].strip()
+					elif len(values) > 0:
+						self.header[values[0]] = ''
+				except:
+					print 'Error in header: %s' % (filename)
+		except:
+			print 'Read error in %s line: %d' % (filename,reader.line_num)
 
 		# Set the local timzone for where the data came from
 		self.local_tz = self.header['DATE'].tzinfo
+		if self.local_tz == None:
+			print "File: %s Unknown TZ: %s" % (filename,dstring)
+			self.local_tz = tz.tzutc()
 
 		# Now read in the data
 		try:
@@ -184,6 +206,7 @@ class PwrLogfile:
 		except:
 			print 'Conversion error in %s line: %d' % (filename,reader.line_num)
 			traceback.print_exc(file=sys.stdout)
+			return False
 
 		self.Tz   = converted_prev[self.SEC]
 		self.ACRz = converted_prev[self.ACR]
@@ -201,24 +224,69 @@ class PwrLogfile:
 		converted.extend(results)
 		data.append(converted)
 
+		power_data_valid = False
+
 		# read the rest of the file
-		for row in reader:
-			if not row:
-				continue
-			try:
-				converted = self.convert_data(row)
-				results,error = self.process_data(converted, converted_prev)
-				if error:
+		try:
+			for row in reader:
+				if not row:
 					continue
+				row_processed = False
+				try:
+					converted = self.convert_data(row)
+					results,error = self.process_data(converted, converted_prev)
+					if error == 2:
+						print '%s : Wattage error line: %d' % (filename,reader.line_num)
+						return False
+					if error == 3:
+						print '%s : Elapsed time error line: %d' % (filename,reader.line_num)
+						return False
+					if error:
+						continue
+				except:
+					print '%s : Conversion error line: %d' % (filename,reader.line_num)
+					continue
+
+				row_processed = True
+				power_data_valid = True
 				self.Wh_sum = results[self.Wh]
+				converted_last_full_interval = converted_prev[:]
 				converted_prev = converted[:]
 				converted.extend(results)
 				data.append(converted)
-			except:
-				print 'Conversion error in %s line: %d' % (filename,reader.line_num)
-		# Add the various init things to the header info for all the net diff calcs
+		except:
+			print '%s : Read error line: %d' % (filename,reader.line_num)
 
-		self.darray = rec.fromrecords(data,names='sec,soc,vb,ib,tb,acr,th,iavg,netacr,deltat,vavg,watts,wh,wavg,tod,zavg')
+		# If the last row of the file is not processed due to short a time period
+		# then back up to the start of the last good interval and process the last entry
+		# using that as the start interval.  This still gives us good results and includes
+		# the final numbers in the counts for net time and net ACR.
+		# This helps up when processing the olpc-batcap logs which have lots of 1 second
+		# samples near the end
+		if not row_processed:
+			# We should have already caught any serious errors when we tried to process it the
+			# first time.  So if it fails for some reason then just ignore it.
+			try:
+				# Back out the Wh sum from the record we are about to replace
+				self.Wh_sum -= self.interval_Wh
+				# Compute the results from the last good intervals start until the
+				# end of the file.
+				results,error = self.process_data(converted, converted_last_full_interval)
+				power_data_valid = True
+				self.Wh_sum = results[self.Wh]
+				converted.extend(results)
+				# Replace the last interval calc with this new one.
+				data[-1] = converted
+			except:
+				pass
+
+		# Add the various init things to the header info for all the net diff calcs
+		self.darray = np.rec.fromrecords(data,names='sec,soc,vb,ib,tb,acr,th,iavg,netacr,deltat,vavg,watts,wh,wavg,tod,zavg')
+
+		if not power_data_valid:
+			return False
+		else:
+			return True
 
 	def set_min_sample_interval(self,interval):
 		self.min_sample_interval=interval
@@ -260,17 +328,54 @@ def histo():
 
 	show()
 
+def filter_data(series,size=5):
+	# we only do odd filter sizes
+	if not (size % 2):
+		size += 1
+
+	center = int(size/2)
+	filtered_data = []
+	filter_series = []
+	flist = series.tolist()
+
+	# Create the data series with the ends extended
+	# so we don't have to special case the fist and last
+	# point.  Repeating the beginning and end values help
+	# weight the start and end points more toward the real
+	# values.
+	for x in xrange(0,center):
+		filter_series.append(flist[0])
+	filter_series.extend(flist[:])
+	# One more item on the end so we don't thow an out
+	# of range index error.
+	for x in xrange(0,center+1):
+		filter_series.append(flist[-1])
+	end = len(filter_series)-center-1
+	for index in xrange(center,end):
+		chunk = sorted(filter_series[index-center:index+center+1])[1:-1]
+		filtered_data.append(np.average(chunk))
+	return filtered_data
+
 def process_logs(filenames,opt):
 	# scatter plots need lists of numbers
 
-	show_avgpwr = 0
-	show_instpwr = 0
-	show_voltcur = 0
-	show_todpwr  = 0
-	save_graphs  = 1
-	dont_show    = 0
-	show_volt    = 0
-	show_zavg    = 1
+	show_avgpwr 		= opt.avgpwr
+	show_instpwr 		= opt.pwr
+	show_voltcur 		= 1
+	show_todpwr  		= 0
+	save_graphs  		= opt.pdf
+	dont_show    		= 0
+	show_volt    		= 0
+	show_zavg    		= 0
+	show_acrhist 		= opt.acrhist or opt.autoacrhist
+	acrhist_autorange 	= opt.autoacrhist
+	show_cur     		= opt.current
+	show_wavg_vs_acr 	= opt.wavgacr
+	show_raw_data 		= opt.raw
+	do_averages 		= 0
+	SKIP 			= opt.skip
+	show_temp 		= opt.temp
+	debug_show_filenames 	= opt.debug
 
 	netacrs = []
 	runtimes = []
@@ -278,6 +383,9 @@ def process_logs(filenames,opt):
 	wavgs	= []
 	pl = PwrLogfile()
 	figures = []
+	average_ib = np.arange(1)
+	average_W  = np.arange(1)
+	nr_data_series = 0
 
 	pl.set_min_sample_interval(opt.compress)
 
@@ -305,7 +413,10 @@ def process_logs(filenames,opt):
 		ax3 = fig3.add_subplot(111)
 		ax3.set_xlabel('Delta Time (Hours)')
 		ax3.set_ylabel('Inst Power (Watts)')
-		ax3.set_title('Power vs Delta Time' )
+		if show_raw_data:
+			ax3.set_title('Power vs Time' )
+		else:
+			ax3.set_title('Power (filtered) vs Time' )
 		figures.append(fig3)
 
 	if show_voltcur:
@@ -335,16 +446,66 @@ def process_logs(filenames,opt):
 		ax7.set_title('Impeadance vs Time' )
 		figures.append(fig7)
 
-	SKIP = 1
+	if show_acrhist:
+		fig8= figure()
+		ax8 = fig8.add_subplot(111)
+		ax8.set_title('Capacity Histogram')
+		ax8.set_xlabel('mAh')
+		ax8.set_ylabel('Runs (nomalized)')
+		if not acrhist_autorange:
+	#		majorTick = MultipleLocator(0)
+			minorTick = MultipleLocator(10)
+	#		ax8.xaxis.set_major_locator(majorTick)
+			ax8.xaxis.set_minor_locator(minorTick)
+		ax8.grid()
+		figures.append(fig8)
+	if show_cur:
+		fig9=figure()
+		ax9 = fig9.add_subplot(111)
+		if show_raw_data:
+			ax9.set_title('Current vs Time')
+		else:
+			ax9.set_title('Current (filtered) vs Time')
+		ax9.set_xlabel('Delta Time (Hours)')
+		ax9.set_ylabel('Current (mA)')
+		figures.append(fig9)
+
+	if show_wavg_vs_acr:
+		fig10=figure()
+		ax10 = fig10.add_subplot(111)
+		ax10.set_title('Average Wattage vs Net Acr')
+		ax10.set_ylabel('Net ACR')
+		ax10.set_xlabel('Average Wattage')
+		ax10.grid()
+		figures.append(fig10)
+
+	if show_temp:
+		fig11 = figure()
+		ax11 = fig11.add_subplot(111)
+		ax11.set_title('Battery Temperature vs Time' )
+		ax11.set_ylabel('Battery Temperature degC')
+		ax11.set_xlabel('Delta Time (Hours)')
+
 	for filename in filenames:
-		pl.read_file(filename)
+		read_result = pl.read_file(filename)
+
+		if debug_show_filenames:
+			print filename
+			if not read_result:
+				print "Skipped because of data error"
+
+		if not read_result:
+			continue
 
 		if show_avgpwr:
 			ax.plot(pl.darray.th[SKIP:],pl.darray.wavg[SKIP:])
 
 		if show_instpwr:
-			ax3.plot(pl.darray.th[SKIP:],pl.darray.watts[SKIP:])
-
+			if show_raw_data:
+				ax3.plot(pl.darray.th[SKIP:],pl.darray.watts[SKIP:])
+			else:
+				mavg = filter_data(pl.darray.watts[SKIP:])
+				ax3.plot(pl.darray.th[SKIP:],mavg)
 		if show_voltcur:
 			ax4.plot(pl.darray.th[SKIP:],pl.darray.vb[SKIP:])
 			ax4_2.plot(pl.darray.th[SKIP:],pl.darray.ib[SKIP:])
@@ -359,35 +520,52 @@ def process_logs(filenames,opt):
 			ax6.plot(pl.darray.th[SKIP:],pl.darray.vb[SKIP:])
 		if show_zavg:
 			ax7.plot(pl.darray.th[SKIP:],pl.darray.zavg[SKIP:])
+		if show_cur:
+			if show_raw_data:
+				ax9.plot(pl.darray.th[SKIP:],pl.darray.ib[SKIP:])
+			else:
+				mavg = filter_data(pl.darray.ib[SKIP:],5)
+				ax9.plot(pl.darray.th[SKIP:],mavg)
 
+		if show_wavg_vs_acr:
+			ax10.scatter(abs(pl.darray.wavg[-1]),abs(pl.darray.netacr[-1]))
 
-#		ax6.set_title('Runtime vs Batt Temp' )
-#		ax6.plot(pl.darray.th[SKIP:],pl.darray.tb[SKIP:])
+		if show_temp:
+			ax11.plot(pl.darray.th[SKIP:],pl.darray.tb[SKIP:])
+
 
 		runtimes.append(pl.darray.th[-1])
 		netacrs.append(pl.darray.netacr[-1])
 		whs.append(pl.darray.wh[-1]*-1)
 		wavgs.append(pl.darray.wavg[-1]*-1)
 
-#	ax.set_title('Wavg vs netACR')
-#	ax.scatter(wavgs,netacrs)
-#	ax2.scatter(runtimes,netacrs)
-#	plt.figure()
-#	ax2.scatter(runtimes,whs)
-#	plt.figure()
-#	plt.scatter(runtimes,wavgs)
-#	plt.figure()
-#	plt.hist(wavgs,10, normed=False, facecolor='green')
+	if show_acrhist:
+		abs_acr = [abs(x) for x in netacrs]
+		mu    = np.mean(abs_acr)
+		stdev = np.std(abs_acr)
+		if acrhist_autorange:
+			# 30 bins but let matplotlib sort out the range.
+			counts, bins, patches = ax8.hist(abs_acr,30,normed=True, facecolor='g',alpha=.75)
+		else:
+			# Scale the histogram so we can compare different graphs easily.
+			counts, bins, patches = ax8.hist(abs_acr,30,range=(2700,3000), normed=True, facecolor='g',alpha=.75)
+			ax8.set_ylim(0,.030)
+		# Set the ticks to be at the edges of the bins
+#		ax8.set_xticks(bins)
+#		labels = ax8.get_xticklabels()
+#		for label in labels:
+#			label.set_rotation(40)
+		npdf  = normpdf(bins, mu, stdev)
+		ax8.plot(bins,npdf,'k--',linewidth=1.5)
+
+	if save_graphs:
+		for each in figures:
+			pp.savefig(each)
+		pp.close()
 
 	show()
 
-	if save_graphs:
-		for each in figures: pp.savefig(each)
-		pp.close()
-
-
 def main():
-# 	filenames = sys.argv[1:]
 
 	parser = argparse.ArgumentParser(description='Make power log graph')
 	parser.add_argument('filenames', nargs='+', help='files to process')
@@ -395,10 +573,36 @@ def main():
 		help='Minumum number of elapsed seconds for each datapoint')
 	parser.add_argument('--chg_limit',type=float,
 		help='Limit charge wattage')
+	parser.add_argument('--debug', action='store_true',default=False,
+		help='Enabled debug output')
+	parser.add_argument('--pdf', action='store_true',default=False,
+		help='Create pdf of all graphs')
+	parser.add_argument('--current', action='store_true',default=False,
+		help='Plot current vs time')
+	parser.add_argument('--wavgacr', action='store_true',default=False,
+		help='Plot average wattage vs net ACR')
+	parser.add_argument('--raw', action='store_true',default=False,
+		help="Don't filter the data")
+	parser.add_argument('--avgpwr', action='store_true',default=False,
+		help="Output average power for all series")
+	parser.add_argument('--acrhist', action='store_true',default=False,
+		help="Show net ACR histogram")
+	parser.add_argument('--skip', action='store',type=int,default=1,
+		help='Number of data points from the start to skip when plotting')
+	parser.add_argument('--autoacrhist', action='store_true',default=False,
+		help="Allow acr histogram to autorange")
+	parser.add_argument('--temp', action='store_true',default=False,
+		help="Plot battery temperature vs time")
+	parser.add_argument('--pwr', action='store_true',default=False,
+		help="Plot instant power vs time")
+
 
 	args = parser.parse_args()
 
-	process_logs(args.filenames,args)
+	if len(args.filenames) != 0:
+	    process_logs(args.filenames,args)
+	else:
+	    print "No files found to process"
 
 main()
 
